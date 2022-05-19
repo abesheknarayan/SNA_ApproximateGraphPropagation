cf3c65a6a9fcc8390c768434da4e53bc0af27f00
abesheknarayan <abeshek.ramanathan2000@gmail.com>
2022-05-19 19:44:23 +0530

    [Add]: incomplete randomized prop. algo and secure pagerank

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+import time
 from pagerank import page_rank_using_basic_propagation
 from data_processing import load_graph_dataset
+from secure.test_pagerank import testScatterAndGatherContactTracing
+import matplotlib.pyplot as plt
 
 import numpy as np
 
 def main():
     graph,n = load_graph_dataset()
-    pi = page_rank_using_basic_propagation(graph,n)
-    print(len(np.unique(pi)))
+    pagerank_metric_x = []
+    pagerank_metrix_y = []
+    for i in [1,2,3,4]:
+        st_time = time.time()
+        pi = page_rank_using_basic_propagation(graph,n,i)
+        en_time = time.time()
+        print(i,en_time-st_time)
+        pagerank_metric_x.append(10*i)
+        pagerank_metrix_y.append(en_time-st_time)
+    plt.plot(pagerank_metric_x,pagerank_metrix_y)
+    plt.xlabel("No of iterations")
+    plt.ylabel("Time (in seconds)")
+    plt.savefig('output/pagerank_metric.png')
+
+    # print(len(np.unique(pi)))
+
+    print("...doing secure pagerank...")
+
+    testScatterAndGatherContactTracing()
+
 
 
 if __name__ == '__main__':
--- pagerank.py
+++ pagerank.py
@@ -8,11 +8,11 @@ e = 10**(-7) # margin of error
 a = 0
 b = 1
 t = 5        # poisson distribution parameter
-L = 5        # number of iterations
+# L = 5        # number of iterations
 alpha = 0.85 # google's pagerank parameter
 
 
-def page_rank_using_basic_propagation(graph,n):
+def page_rank_using_basic_propagation(graph,n,L):
     print('...starting PageRank using basic graph propagation...')
     m = graph.number_of_edges()
     print(n,m)
@@ -47,30 +47,39 @@ def page_rank_using_basic_propagation(graph,n):
 
 
 
-# def gnn_node_classification_using_random_propagation(graph):
-#     print('...starting PageRank using randomized graph propagation...')
+def gnn_node_classification_using_random_propagation(graph):
+    print('...starting PageRank using randomized graph propagation...')
 
-#     X = np.array([1.0/n for i in range(n)])  # doubtful whether all are 1/n or only the seed node's value is 1/n
+    X = np.array([1.0/n for i in range(n)])  # doubtful whether all are 1/n or only the seed node's value is 1/n
     
-#     W = np.array([alpha*(pow(1.0-alpha,i)) for i in range(n+1)]) # n+1 because we access Y[i+1] later edge condition
+    W = np.array([alpha*(pow(1.0-alpha,i)) for i in range(n+1)]) # n+1 because we access Y[i+1] later edge condition
 
-#     Y = W.cumsum()
+    Y = W.cumsum()
 
-#     r = np.array(X)   # initially r = x 
-#     r_ = np.zeros(r.shape) # extra temporary vector to store next level's r vector
+    r = np.array(X)   # initially r = x 
+    r_ = np.zeros(r.shape) # extra temporary vector to store next level's r vector
     
-#     q = np.zeros(r.shape)
+    q = np.zeros(r.shape)
 
-#     pi = np.zeros(r.shape)
+    pi = np.zeros(r.shape)
 
-#     for i in range(L):
-#         for u in graph.nodes:
-#             if r[u] != 0:
-#                 for v in graph[u]:
-#                     # extra condition
-#                     if graph.degree[v] <= pow(((1/e)*(Y[i+1]/Y[i])*(r[u]/pow(graph.degree[u],b))),1/a):
-#                         r_[v] = r_[v] + ((Y[i+1]/Y[i])*(r[u]))/((pow(graph.degree[v],a))*(pow(graph.degree[u],b)))
-#                     else:
-#                         # do subset sampling
-                    
+    for i in range(L):
+        for u in graph.nodes:
+            if r[u] != 0:
+                for v in graph[u]:
+                    # extra condition
+                    if graph.degree[v] <= pow(((1/e)*(Y[i+1]/Y[i])*(r[u]/pow(graph.degree[u],b))),1/a):
+                        r_[v] = r_[v] + ((Y[i+1]/Y[i])*(r[u]))/((pow(graph.degree[v],a))*(pow(graph.degree[u],b)))
+                    else:
+                        # do subset sampling
+                        ...
+                q[u] = (W[i]*r[u])/Y[i]
+        pi = pi + q
+        r = r_
+        r_ = np.zeros(r.shape)
+        q = np.zeros(r.shape) 
+        q = np.array([(W[L]/Y[L])*r[i] for i in range(n)])
+        pi = pi + q
+        return pi   
+
 
